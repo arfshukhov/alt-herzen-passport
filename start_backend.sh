778c62f4e9f529e1b9e7762f5bfae6fda7cc9248
Vladimir Shukhov <81926456+arfshukhov@users.noreply.github.com>
2024-10-24 17:23:23 +0300

    Add files via upload

--- start_backend.sh
+++ start_backend.sh
@@ -3,7 +3,6 @@
 export HOST=127.0.0.1
 export PORT=8888
 
-pip install -r requirements.txt
+pip install -r src/requirements.txt
 
-
-gunicorn --bind=$HOST:$PORT "backend.app:app"
+gunicorn --bind=$HOST:$PORT "src.app:app"
